d2aea5e28e8bf9fb6fa425e983bd04d2f224c52d
Mathieu Lecarme <mlecarme@bearstech.com>
2020-07-30 22:34:56 +0200

    REST as POST.

--- services/illustrate_that_for_me/service.js
+++ services/illustrate_that_for_me/service.js
@@ -3,38 +3,40 @@ const jwt = require('express-jwt');
 const mashup = require('./mashup');
 
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "127.0.0.1";
 
-app.get("/pict",
-    jwt({
-        secret: process.env.SECRET,
-        algorithms: ['HS256'],
-        issuer: 'cms',
-        credentialsRequired: true,
-        getToken: (req) => {
-            console.log("secret", process.env.SECRET);
-            if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-                console.log("authorization", req.headers.authorization);
-                return req.headers.authorization.split(' ')[1];
-            } else if (req.query && req.query.token) {
-            console.log("token", req.query.token);
-              return req.query.token;
-            }
-            return null;
-        },
-    }),
-    async (req, res, next) => {
-        if (! ("txt" in req.query)) {
-            res.status(400).end();
-            return;
-        }
-        try {
-            res.json(await mashup.illustrate_that_for_me(req.query.txt));
-        } catch(error) {
-            return next(error);
-        }
-});
+app.post(
+  "/pict",
+  jwt({
+    secret: process.env.SECRET,
+    algorithms: ["HS256"],
+    issuer: "cms",
+    credentialsRequired: true,
+    getToken: (req) => {
+      if (
+        req.headers.authorization &&
+        req.headers.authorization.split(" ")[0] === "Bearer"
+      ) {
+        return req.headers.authorization.split(" ")[1];
+      }
+      return null;
+    },
+  }),
+  async (req, res, next) => {
+    if (!("txt" in req.body)) {
+      res.status(400).end();
+      return;
+    }
+    try {
+      res.json(await mashup.illustrate_that_for_me(req.body.txt));
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
 
 app.listen(port, host, () =>
     console.log(`App listening at http://${host}:${port}`));
