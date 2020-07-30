c5cbdd2d34ec7048c874b27a738a21bd0d1a2ebb
Mathieu Lecarme <mlecarme@bearstech.com>
2020-07-30 19:22:25 +0200

    Now is enough.

--- services/cms/index.php
+++ services/cms/index.php
@@ -16,7 +16,7 @@ $token = (new Builder())->issuedBy('cms') // Configures the issuer (iss claim)
                         ->permittedFor('http://' . $_SERVER['HTTP_HOST']) // Configures the audience (aud claim)
                         ->identifiedBy($id, true) // Configures the id (jti claim), replicating as a header item
                         ->issuedAt($time) // Configures the time that the token was issue (iat claim)
-                        ->canOnlyBeUsedAfter($time) // Configures the time that the token can be used (nbf claim)
+                        //->canOnlyBeUsedAfter($time) // Configures the time that the token can be used (nbf claim)
                         ->expiresAt($time + 3600) // Configures the expiration time of the token (exp claim)
                         ->withClaim('uid', 1) // Configures a new claim, called "uid"
                         ->getToken($signer, $key); // Retrieves the generated token
