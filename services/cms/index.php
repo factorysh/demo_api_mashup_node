f17ce7ce91a099c099265ffd26e8e6de46027826
Mathieu Lecarme <mlecarme@bearstech.com>
2020-07-30 22:33:36 +0200

    Removing jti and uuid.

--- services/cms/index.php
+++ services/cms/index.php
@@ -4,17 +4,15 @@ require __DIR__ . '/vendor/autoload.php';
 use Lcobucci\JWT\Builder;
 use Lcobucci\JWT\Signer\Key;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
-use Ramsey\Uuid\Uuid;
 
 $key = new Key($_SERVER['SECRET']);
 
 $signer = new Sha256();
 $time = time();
-$id = Uuid::uuid4();
 
 $token = (new Builder())->issuedBy('cms') // Configures the issuer (iss claim)
                         ->permittedFor('http://' . $_SERVER['HTTP_HOST']) // Configures the audience (aud claim)
-                        ->identifiedBy($id, true) // Configures the id (jti claim), replicating as a header item
+                        //->identifiedBy($id, true) // Configures the id (jti claim), replicating as a header item
                         ->issuedAt($time) // Configures the time that the token was issue (iat claim)
                         //->canOnlyBeUsedAfter($time) // Configures the time that the token can be used (nbf claim)
                         ->expiresAt($time + 3600) // Configures the expiration time of the token (exp claim)
